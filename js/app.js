6507e90d24d9b6aeeb3ab7c13c815a6d427421d7
Mohammad Yaseen <yaseenleo>
2018-07-24 13:39:51 +0500

    footer, navbar sections responsiveness improved

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $(".sidebarBtn").click(function(){
-        $(".sidebar").toggleClass(".active")
-    })
-})
+function openNav() {
+        
+    document.getElementById("side_bar").style.width = "300px";
+    document.getElementById("main").style.marginRight = "300px";
+   // document.getElementById("main").style.border = "10px solid red";
+
+}
+
+function closeNav() {
+    document.getElementById("side_bar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
+
+$("#")
